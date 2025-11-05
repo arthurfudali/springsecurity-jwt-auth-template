eb468043d0b0e09708a4ea3d42eee13c8cd9ab41
arthurfudali <arthur.fudali@gmail.com>
2025-11-05 14:47:07 -0300

    feat: add inital security config

---
+++ src/main/java/com/dev/arthur/springsecurity_jwt_auth/SpringsecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.dev.arthur.springsecurity_jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityJwtAuthApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dev/arthur/springsecurity_jwt_auth/security/SecurityConfigurations.java
@@ -0,0 +1,48 @@
+package com.dev.arthur.springsecurity_jwt_auth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // isso possibilita a configuração manual do springSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // define as configurações como STATELESS
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN") // para acessar o POST /products precisa ser admin
+                        .anyRequest().authenticated()) // para o resto apenas logado
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { // responsável pela criptografia das senhas
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy(){
+        return RoleHierarchyImpl.fromHierarchy(
+                "ROLE_ADMIN > ROLE_USER\n"
+        );
+    }
+}
